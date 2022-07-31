98ce649f0def6c369e12ad4c93886f92550561fa
Rafael Cena <rafael.cena@hotmail.com>
2022-07-31 11:41:07 -0300

    resolved solid

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -10,6 +10,21 @@ class ListAllUsersUseCase {
 
   execute({ user_id }: IRequest): User[] {
     // Complete aqui
+
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+
+    if(!userAlreadyExists) {
+      throw new Error("User not found!");
+    }
+
+    if(!userAlreadyExists.admin) {
+      throw new Error("You are not an admin!");
+    }
+   
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -10,6 +10,17 @@ class TurnUserAdminUseCase {
 
   execute({ user_id }: IRequest): User {
     // Complete aqui
+
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found!");
+    }
+
+    const updatedUser = this.usersRepository.turnAdmin(user);
+
+    return updatedUser;
+
   }
 }
 
